7e06a76917523796d5ed1de7934a87fc5c2fcbce
tannabr23596 <tabrah13@my.centennialcollege.ca>
2023-10-26 13:58:29 -0400

    First commit for Assignment-2

---
+++ src/main/java/com/teena/assignment2/devops/TeenaAbrahamAssignment2DevopsApplication.java
@@ -0,0 +1,14 @@
+package com.teena.assignment2.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeenaAbrahamAssignment2DevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeenaAbrahamAssignment2DevopsApplication.class, args);
+		System.out.println("Welcome to COMP367");
+	}
+
+}

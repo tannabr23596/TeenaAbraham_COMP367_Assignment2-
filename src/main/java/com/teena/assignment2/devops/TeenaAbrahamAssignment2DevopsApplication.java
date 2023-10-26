5224fce14d8a9f067800ae3689ec0748188b32b8
tannabr23596 <tabrah13@my.centennialcollege.ca>
2023-10-26 15:32:06 -0400

    Updating greeting message and adding name

--- src/main/java/com/teena/assignment2/devops/TeenaAbrahamAssignment2DevopsApplication.java
+++ src/main/java/com/teena/assignment2/devops/TeenaAbrahamAssignment2DevopsApplication.java
@@ -1,5 +1,7 @@
 package com.teena.assignment2.devops;
 
+import java.time.LocalTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +10,17 @@ public class TeenaAbrahamAssignment2DevopsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TeenaAbrahamAssignment2DevopsApplication.class, args);
-		System.out.println("Welcome to COMP367");
+		String name = "Teena Abraham";
+		String greeting = updateGreeting();
+		System.out.println(greeting + ", " + name + ", Welcome to COMP367");
 	}
 
+	public static String updateGreeting() {
+		LocalTime now = LocalTime.now();
+		if (now.isBefore(LocalTime.NOON)) {
+			return "Good morning";
+		} else {
+			return "Good afternoon";
+		}
+	}
 }

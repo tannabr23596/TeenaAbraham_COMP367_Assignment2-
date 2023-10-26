d2b77626aeb68f7d5578b300dbf00bf60acdbda5
tannabr23596 <tabrah13@my.centennialcollege.ca>
2023-10-26 16:01:32 -0400

    Reverting the updating name and greetings

--- src/main/java/com/teena/assignment2/devops/TeenaAbrahamAssignment2DevopsApplication.java
+++ src/main/java/com/teena/assignment2/devops/TeenaAbrahamAssignment2DevopsApplication.java
@@ -1,7 +1,5 @@
 package com.teena.assignment2.devops;
 
-import java.time.LocalTime;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,17 +8,7 @@ public class TeenaAbrahamAssignment2DevopsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TeenaAbrahamAssignment2DevopsApplication.class, args);
-		String name = "Teena Abraham";
-		String greeting = updateGreeting();
-		System.out.println(greeting + ", " + name + ", Welcome to COMP367");
+		System.out.println("Welcome to COMP367");
 	}
 
-	public static String updateGreeting() {
-		LocalTime now = LocalTime.now();
-		if (now.isBefore(LocalTime.NOON)) {
-			return "Good morning";
-		} else {
-			return "Good afternoon";
-		}
-	}
 }
